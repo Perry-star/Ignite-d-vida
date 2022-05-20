a63f62db77b03c3dcdaf4eabe8e8ab50946cbb04
Perry-star <mateus.ramosilva@gmail.com>
2022-05-20 20:28:47 -0300

    First Commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,213 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+import { useFocusEffect } from '@react-navigation/core';
+import { useTheme } from 'styled-components';
+
+import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
+
+
+import { 
+    Container,
+    Header,
+    User,
+    UserInfo,
+    Photo,
+    UserGretting,
+    UserName,
+    UserWrapper,
+    Icon,
+    HighlightCards,
+    Transactions, 
+    Title,
+    TransactionList,
+    LogoutButton,
+    LoadContainer,
+    } from './styles';
+import AsyncStorageLib from '@react-native-async-storage/async-storage/jest/async-storage-mock';
+
+    export interface DataListProps extends TransactionCardProps{
+        id: string;
+    }
+interface HighlightProps{
+    amount: string;
+}    
+
+interface HighlightData{
+    entries: HighlightProps;
+    expensives: HighlightProps;
+    total: HighlightProps;
+
+}
+
+export function Dashboard(){
+   const [isLoading, setIsLoading] = useState(true); 
+   const [transactions, setTransactions] = useState<DataListProps[]>([]);
+   const [hightlightData, setHightlightData] = useState<HighlightData>({} as  HighlightData);
+
+   const theme = useTheme();
+
+   async function loadTransaction(){
+    const dataKey = '@gofinances:transactions';
+    const response = await AsyncStorage.getItem(dataKey);
+
+    const transactions = response ? JSON.parse(response) : [];
+
+    let entriesTotal = 0;
+    let expensiveTotal = 0;
+
+    const transactionsFormatted: DataListProps[] = transactions
+    .map((item: DataListProps) => {
+
+        if(item.type === 'positive'){
+            entriesTotal += Number(item.amount);
+        }else {
+            expensiveTotal += Number(item.amount);
+        }
+        
+        const amount = Number(item.amount)
+        .toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+
+        
+        const date = Intl.DateTimeFormat('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit'
+        }).format(new Date(item.date));
+
+        return {
+            id: item.id,
+            name: item.name,
+            amount,
+            type: item.type,
+            category: item.category,
+            date,
+        }
+    });
+
+    setTransactions(transactionsFormatted);
+
+    const total = entriesTotal - expensiveTotal;
+
+    setHightlightData({
+        entries: {
+            amount: entriesTotal.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL'
+            })
+        },
+        expensives: {
+            amount: expensiveTotal.toLocaleString('pt-BR',{
+                style: 'currency',
+                currency: 'BRL'
+            })
+        },
+        total: {
+            amount: total.toLocaleString('pt-BR',{
+                style: 'currency',
+                currency: 'BRL'
+            })
+
+        }
+    });
+    
+    setIsLoading(false);
+   
+   } 
+
+   useEffect(() => {
+    loadTransaction();
+   },[]);
+
+   useFocusEffect(useCallback(() => {
+       loadTransaction();
+   }, []));
+
+ return(
+    <Container>
+      { 
+  
+            isLoading ?  
+            <LoadContainer>
+               <ActivityIndicator 
+                color={theme.colors.primary}
+                size='large'
+                /> 
+            </LoadContainer>:
+            <>
+                <Header>
+                    <UserWrapper>
+                        <UserInfo>
+                            <Photo 
+                                source={{uri:'https://avatars.githubusercontent.com/u/82467764?v=4' }}
+                            />
+                            <User>
+                                <UserGretting>Olá,</UserGretting>
+                                <UserName>Matheus</UserName>
+                            </User>
+                        </UserInfo>
+
+                        <LogoutButton onPress={() => {}}>
+                            <Icon name="power"/>
+                        </LogoutButton>
+                            
+        
+                </UserWrapper>
+                
+                </Header> 
+
+                <HighlightCards>
+                    
+                    <HighlightCard 
+                    type="up"
+                    title="Entradas" 
+                    amount={hightlightData.entries.amount} 
+                    lastTransaction="Última entrada dia 13 de abril"
+                    />
+                    <HighlightCard 
+                    type="down"
+                    title="Saídas" 
+                    amount={hightlightData.expensives.amount} 
+                    lastTransaction="Última saída dia 03 de abril" 
+                    />
+
+                    <HighlightCard 
+                    type="total"
+                    title="Total" 
+                    amount={hightlightData.total.amount}
+                    lastTransaction="01 á 16 de abril"
+                    />
+
+                </HighlightCards>
+
+                <Transactions>
+                    <Title>Listagem</Title>
+
+                    <TransactionList
+                        data={transactions}
+                        keyExtractor={item => item.id}
+                        renderItem = {({ item }) => <TransactionCard data={item} /> }
+                        
+                    
+                    />
+            
+                    
+
+                
+                </Transactions>
+            </>
+        }
+
+    </Container>
+        
+    )
+}
+
+
+
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,249 @@
+import React, { useState} from 'react';
+import { 
+    Keyboard, 
+    Modal, 
+    TouchableWithoutFeedback,
+    Alert,
+
+} from 'react-native';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
+import { useForm } from 'react-hook-form';
+import { CommonActions, useNavigation } from '@react-navigation/native';
+
+
+import { InputForm } from '../../components/Form/InputForm';
+import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelectButton  } from '../../components/Form/CategorySelectButton';
+
+
+
+import { CategorySelect } from '../CategorySelect';
+
+ import { 
+     Container,
+     Header,
+     Title,
+     Form,
+     Fields,
+     TransactionTypes,
+     
+} from './styles';
+
+interface FormData {
+    name: string;
+    amount: string;
+}
+
+const schema = Yup.object().shape({
+    name:Yup
+    .string()
+    .required('Nome é Obrigatorio'),
+    amount: Yup
+    .number()
+    .typeError(' Informe um valor númerico ')
+    .positive('O valor não pode ser negativo')
+    .required('O valor é obrigatório')
+
+}); // yup é uma biblioteca que serve para validação de formulario 
+
+export function Register () {
+    const [TransactionType, setTransactionType] = useState('');
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+
+   
+
+    const [category, setCategory] = useState({
+        key: 'category',
+        name: 'Categoria',
+        
+    });
+    
+   const navigation = useNavigation();
+    
+
+    const {
+        control,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    });
+    
+   /*const [ name, setName] = useState('');
+    const [ amount, setAmount ] = useState(''); */
+
+    function handleNavigationToListagem(){
+        navigation.dispatch(
+            CommonActions.navigate({
+                name: 'Listagem',
+            })
+        );
+    }
+   
+
+    function handleTransactionsTypeSelect(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+        
+    
+
+    function handleOpenSelectCategoryModal(){
+        setCategoryModalOpen(true);
+    }
+
+    function handleCloseSelectCategoryModal() {
+        setCategoryModalOpen(false);
+    }
+
+ async function handleRegister(form: FormData){
+        if(!TransactionType)
+        return Alert.alert('Selecione o tipo da transação');//Se na tela do formulario o usuario não colocar nada enviara uma mensagem
+
+        if(category.key === 'category')
+        return Alert.alert('Selecione a categoria'); //Se na tela do formulario o usuario não colocar nada enviara uma mensagem
+
+        const newTransaction = {
+           id: String(uuid.v4()), 
+           name: form.name,
+           amount: form.amount,
+           TransactionType,
+           category: category.key
+
+        }
+        
+        try {
+            const dataKey = '@gofinances:transactions';
+
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
+
+            const dataFormatted = [
+                ...currentData,
+                newTransaction
+        ];
+
+        await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+        reset();
+        setTransactionType('');
+        setCategory({
+            key: 'category',
+            name: 'Categoria'
+        });
+
+        //navigation.navigate("Listagem");
+
+        handleNavigationToListagem();
+
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Não foi possível salvar");
+    
+        }
+     
+
+    }
+
+    /*useEffect(() => {
+        async function loadData(){
+            const data = await AsyncStorage.getItem(dataKey);
+            console.log(JSON.parse(data));
+        }
+
+        loadData();
+       
+    },[])*/
+
+    
+
+
+    return(
+
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}> 
+        
+        <Container>
+           
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+            
+            
+        <Form>
+            <Fields>
+                <InputForm
+                    name="name"
+                    control={control}
+                    placeholder="Nome" 
+                    placeholderTextColor="grey"
+                    autoCapitalize="sentences" // define como quero colocar em maiusculo cada palavra/sentenca
+                    autoCorrect={false} //serve para corrigir palavras
+                    error={errors.name && errors.name.message}
+
+                    //onChangeText={setName} // ou text => setname(text)
+
+                />
+                <InputForm
+                    name="amount"
+                    control={control}
+                    placeholder="Preço" 
+                    placeholderTextColor="grey"
+                    keyboardType="numeric" //muda o teclado ou para texto ou para númerico dependendo da necessidade
+                    error={errors.amount && errors.amount.message}
+                    //onChangeText={setAmount}
+
+                />
+                <TransactionTypes>
+                    <TransactionTypeButton
+                        type="up"
+                        title="Income"
+                        onPress={() => handleTransactionsTypeSelect('up')}
+                        isActive={TransactionType === 'up'}
+                    />
+                    <TransactionTypeButton
+                        type="down"
+                        title="Outcome"
+                        onPress={() => handleTransactionsTypeSelect('down')}
+                        isActive={TransactionType === 'down'}
+                    />
+                </TransactionTypes> 
+
+                <CategorySelectButton
+                    title={category.name}
+                    onPress={handleOpenSelectCategoryModal}
+                />   
+
+           
+            </Fields>
+            <Button 
+                title="Enviar"
+                onPress={handleSubmit(handleRegister)}
+            />
+            
+           </Form>
+
+           <Modal visible={categoryModalOpen}>
+               <CategorySelect
+                    category={category}
+                    setCategory={setCategory}
+                    closeSelectCategory={handleCloseSelectCategoryModal}
+
+               />
+
+               
+           </Modal>
+
+         
+            
+        
+
+        </Container>
+    </TouchableWithoutFeedback> //Ao tocar em qualquer ambiente na tela o teclado fecha
+
+    );
+}
